37a3d604e1994b8b39a22cd9b98618bad476e712
SHT-3756 <gkdlfndkekd@naver.com>
2025-01-30 20:35:26 +0900

    feat : 로그인 UI 추가

--- lib/app/domain/user/views/sign_in_screen.dart
+++ lib/app/domain/user/views/sign_in_screen.dart
@@ -1,12 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:subject/app/domain/user/controller/auth_controller.dart';
 
-class SignInScreen extends StatelessWidget {
-  const SignInScreen({Key? key}) : super(key: key);
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("로그인"),
+    return Scaffold(
+      appBar: AppBar(title: const Text("회원가입")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: "이메일",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                labelText: "비밀번호",
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  await AuthController.to.signIn(emailController.text.trim(),
+                      passwordController.text.trim());
+                  Get.offNamed('/todo');
+                } catch (e) {
+                  print(e);
+                  Get.snackbar("Error", e.toString());
+                }
+              },
+              child: const Text("로그인"),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  await AuthController.to.register(emailController.text.trim(),
+                      passwordController.text.trim());
+                  Get.offNamed('/todo');
+                } catch (e) {
+                  print(e);
+                  Get.snackbar("Error", e.toString());
+                }
+              },
+              child: const Text("회원가입"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
