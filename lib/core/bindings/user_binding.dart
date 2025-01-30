75fde1aed1605593b22e819b167fdcbfce922497
SHT-3756 <gkdlfndkekd@naver.com>
2025-01-30 17:17:24 +0900

    delete : 삭제

--- lib/core/bindings/user_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:subject/app/domain/user/controller/user_controller.dart';
-
-class UserBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<UserController>(() => UserController());
-  }
-}
