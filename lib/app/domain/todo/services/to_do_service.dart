f5a1235850e68d8a804a29c513ac445ddf24f7f3
SHT-3756 <gkdlfndkekd@naver.com>
2025-01-30 18:50:54 +0900

    feat : 삭제 service 추가, fetchTasks where 문 추가

--- lib/app/domain/todo/services/to_do_service.dart
+++ lib/app/domain/todo/services/to_do_service.dart
@@ -5,12 +5,13 @@ class ToDoService {
   final FirebaseFirestore _fb = FirebaseFirestore.instance;
 
   Future<List<ToDoModel>> fetchTasks() async {
-    final snapshot = await _fb.collection('tasks').get();
+    final snapshot = await _fb.collection('tasks').where('isDelete', isNotEqualTo: true).get();
     return snapshot.docs.map((doc) => ToDoModel.fromJson(doc.data())).toList();
   }
 
   Future<bool> createTask(ToDoModel todo) async {
-    final DocumentReference taskRef = _fb.collection('tasks-info').doc('detail');
+    final DocumentReference taskRef =
+        _fb.collection('tasks-info').doc('detail');
     try {
       await _fb.runTransaction((transaction) async {
         final taskSnapshot = await transaction.get(taskRef);
@@ -26,7 +27,6 @@ class ToDoService {
         int newWeight = lastWeight + 100;
         int newLastKanName = lastKanName + 1;
 
-
         final newTaskRef = _fb.collection('tasks').doc('KAN-$newLastKanName');
 
         transaction.set(newTaskRef, {
@@ -37,6 +37,7 @@ class ToDoService {
           'worker': todo.worker,
           'weight': newWeight,
           'createDate': FieldValue.serverTimestamp(),
+          'isDelete': false,
         });
 
         transaction.update(taskRef, {
@@ -46,26 +47,47 @@ class ToDoService {
       });
 
       return true;
-    } catch (e,s) {
+    } catch (e, s) {
       print('createTask Error : $e , $s');
+      return false;
+    }
+  }
+
+  Future<bool> updateTaskDetails(ToDoModel todo) async {
+    try {
+      await _fb.collection('tasks').doc(todo.id).update({
+        'title': todo.title,
+        'content': todo.content,
+        'worker': todo.worker,
+      });
+      return true;
+    } catch (e) {
+      print('updateTaskDetails Error : $e');
       return false;
     }
   }
 
   Future<void> updateTaskStatus(
       String taskId, String newStatus, int newIndex) async {
-    await _fb.collection('tasks').doc(taskId).update({
-      'status': newStatus,
-      'index': newIndex,
-    });
+    // await _fb.collection('tasks').doc(taskId).update({
+    //   'status': newStatus,
+    //   'index': newIndex,
+    // });
   }
 
-  Future<void> updateTaskDetails(
-      String taskId, String title, String content, String worker) async {
-    await _fb.collection('tasks').doc(taskId).update({
-      'title': title,
-      'content': content,
-      'worker': worker,
-    });
+  Future<bool> deleteTask(String id) async {
+    try {
+      await _fb.collection('tasks').doc(id).update({'isDelete': true});
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
+
+// TODO : 삭제 서비스, 컨트롤, 컨펌모달,
+// TODO : 로거 추가
+// TODO : Get.snackbar 왜 안나오는지 체크
+// TODO : 드래그앤 드랍 로직 수정
+// TODO : UI 수정
+// TODO : 노션 정리
 }
