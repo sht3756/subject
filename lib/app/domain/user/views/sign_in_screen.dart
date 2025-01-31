5a5771db099618e98f2de1571c15446b8e9d65fc
SHT-3756 <gkdlfndkekd@naver.com>
2025-01-31 21:12:22 +0900

    fix : 로그인 UI 수정

--- lib/app/domain/user/views/sign_in_screen.dart
+++ lib/app/domain/user/views/sign_in_screen.dart
@@ -10,62 +10,159 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final _formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  late bool isLoginMode = false;
+  late bool isHover = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("회원가입")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                labelText: "이메일",
-                border: OutlineInputBorder(),
+      body: Center(
+        child: Container(
+          width: 400,
+          height: 500,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    isLoginMode ? '회원가입' : '로그인',
+                    style: const TextStyle(fontSize: 25),
+                  ),
+                  const SizedBox(height: 10),
+                  if (isLoginMode) ...[
+                    TextFormField(
+                      keyboardType: TextInputType.name,
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        labelText: "이름",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return '이름을 입력해주세요.';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 4),
+                  ],
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      labelText: "이메일",
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return '이메일을 입력해주세요.';
+                      } else if (!RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}").hasMatch(value)) {
+                        return '올바른 이메일 형식을 입력해주세요.';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 4),
+                  TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      labelText: "비밀번호",
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return '비밀번호를 입력해주세요.';
+                      } else if (value.length < 6) {
+                        return '비밀번호는 6자 이상이어야 합니다.';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 4),
+                  InkWell(
+                    onHover: (bool hover) {
+                      setState(() {
+                        isHover = hover;
+                      });
+                    },
+                    onTap: () async {
+                      if (_formKey.currentState!.validate()) {
+                        try {
+                          if (isLoginMode) {
+                            await AuthController.to.signIn(
+                              emailController.text.trim(),
+                              passwordController.text.trim(),
+                            );
+                          } else {
+                            await AuthController.to.register(
+                              emailController.text.trim(),
+                              passwordController.text.trim(),
+                            );
+                          }
+                          Get.offNamed('/todo');
+                        } catch (e) {
+                          Get.snackbar("Error", e.toString());
+                        }
+                      }
+                    },
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.login,
+                            color: Colors.white,
+                          ),
+                          const SizedBox(width: 10),
+                          Text(
+                            isLoginMode ? '가입하기' : '로그인',
+                            style: const TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(isLoginMode ? '이미 계정이 있습니까?' : '계정이 없습니까?'),
+                      const SizedBox(width: 5),
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            isLoginMode = !isLoginMode;
+                          });
+                        },
+                        child: Text(
+                          isLoginMode ? '여기에서 로그인하세요!' : '여기에서 가입하세요!',
+                          style: const TextStyle(color: Colors.orange),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: passwordController,
-              decoration: const InputDecoration(
-                labelText: "비밀번호",
-                border: OutlineInputBorder(),
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  await AuthController.to.signIn(emailController.text.trim(),
-                      passwordController.text.trim());
-                  Get.offNamed('/todo');
-                } catch (e) {
-                  print(e);
-                  Get.snackbar("Error", e.toString());
-                }
-              },
-              child: const Text("로그인"),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                try {
-                  await AuthController.to.register(emailController.text.trim(),
-                      passwordController.text.trim());
-                  Get.offNamed('/todo');
-                } catch (e) {
-                  print(e);
-                  Get.snackbar("Error", e.toString());
-                }
-              },
-              child: const Text("회원가입"),
-            ),
-          ],
+          ),
         ),
       ),
     );
