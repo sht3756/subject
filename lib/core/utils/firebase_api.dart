1f00b8a5c294618c567b24b7c368a16affde4eba
SHT-3756 <gkdlfndkekd@naver.com>
2025-02-03 01:24:28 +0900

    build test

---
+++ lib/core/utils/firebase_api.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseApi {
+  final FirebaseFirestore fb = FirebaseFirestore.instance;
+
+
+}
