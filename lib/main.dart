47bb94ed58519d6cc91566476a6996cd34fb5bd6
SHT-3756 <gkdlfndkekd@naver.com>
2025-02-02 22:48:20 +0900

    fix : Getx 로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
+import 'package:subject/app/domain/user/controller/auth_controller.dart';
 import 'package:subject/core/utils/firebase_config.dart';
 
+import 'core/bindings/auth_binding.dart';
 import 'core/routes/app_route.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
   await Firebase.initializeApp(options: FirebaseConfig.options);
+  Get.put(AuthController());
   runApp(const MyApp());
 }
 
@@ -17,11 +21,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TODO',
-      theme: ThemeData(primaryColor: Colors.blue),
-      routerConfig: AppRouter.router,
+      initialBinding: AuthBinding(),
+      initialRoute: AuthController.to.getInitialRoute(),
+      getPages: AppRouter.pageLists,
+      defaultTransition: Transition.noTransition,
     );
   }
 }
